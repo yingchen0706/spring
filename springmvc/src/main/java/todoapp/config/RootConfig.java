1bed2eb8d0ac02aeb42c42c38fa85546c55ea59f
Ying Chen <yiyichen@oradev.oraclecorp.com>
2018-05-04 03:11:23 -0600

    update

--- springmvc/src/main/java/todoapp/config/RootConfig.java
+++ springmvc/src/main/java/todoapp/config/RootConfig.java
@@ -1,5 +1,16 @@
 package todoapp.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages= {"todoapp"},
+		excludeFilters= {
+				@Filter(type=FilterType.ANNOTATION, value=EnableWebMvc.class)
+		})
 public class RootConfig {
 
 }
--- springmvc/src/main/java/todoapp/config/TodoAppWebAppInitializer.java
+++ springmvc/src/main/java/todoapp/config/TodoAppWebAppInitializer.java
@@ -7,19 +7,19 @@ public class TodoAppWebAppInitializer extends AbstractAnnotationConfigDispatcher
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		// TODO Auto-generated method stub
-		return null;
+		return new Class<?>[] {RootConfig.class};
 	}
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
 		// TODO Auto-generated method stub
-		return null;
+		return new Class<?>[] {WebConfig.class};
 	}
 
 	@Override
 	protected String[] getServletMappings() {
 		// TODO Auto-generated method stub
-		return null;
+		return new String[] {"/"};
 	}
 
 }
